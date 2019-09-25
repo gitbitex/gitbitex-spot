7e3cb4766cf74b567160aafa12160f21350901e9
greensheng <lingqing.wan@matrixport.com>
2019-09-25 21:48:38 +0800

    -

--- worker/tick_maker.go
+++ worker/tick_maker.go
@@ -15,7 +15,6 @@
 package worker
 
 import (
-	"fmt"
 	"github.com/gitbitex/gitbitex-spot/matching"
 	"github.com/gitbitex/gitbitex-spot/models"
 	"github.com/gitbitex/gitbitex-spot/service"
@@ -77,9 +76,6 @@ func (t *TickMaker) OnDoneLog(log *matching.DoneLog, offset int64) {
 func (t *TickMaker) OnMatchLog(log *matching.MatchLog, offset int64) {
 	for _, granularity := range T {
 		startPos := log.Time.UTC().Truncate(time.Duration(granularity) * time.Minute).Unix()
-		if t.logReader.GetProductId() == "BTC-USDT" && granularity == 1440 {
-			fmt.Printf("%v %v", log.Time, startPos)
-		}
 
 		tick, found := t.ticks[granularity]
 		if !found || tick.Time != startPos {
