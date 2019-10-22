b5f71e3e34164931de9dab64ecd2a528b7248e8c
greensheng <lingqing.wan@matrixport.com>
2019-10-22 12:18:28 +0800

    -

--- service/user_service.go
+++ service/user_service.go
@@ -98,10 +98,10 @@ func CheckToken(tokenStr string) (*models.User, error) {
 		return nil, err
 	}
 	if user == nil {
-		return nil, errors.New("bad token: 0x1")
+		return nil, errors.New("bad token")
 	}
 	if user.PasswordHash != passwordHash {
-		return nil, errors.New("bad token: 0x2")
+		return nil, errors.New("bad token")
 	}
 	return user, nil
 }
